a50999e4f94e64abfc4ae8e295469edf5a662f18
Maria Petrova <maria-petrova1296@yandex.ru>
2024-05-17 19:25:39 +0300

    Вынесен запуск сервера в отдельную функцию. Добавлена обработка сигналов.

--- backend/main.go
+++ backend/main.go
@@ -1,28 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
-
-	"github.com/Mary-cross1296/go_final_project/tests"
+	"syscall"
 )
+
+func HttpServer(port, wd string, server *http.Server) {
+	//var httpServer *http.Server
+	// Определение обработчика для корневого пути
+	requestHandler := http.FileServer(http.Dir(wd))
+
+	// Настройка сервера
+	server.Handler = requestHandler
+
+	// Запуск сервера на указанном порту
+	log.Printf("Сервер запущен на порту %v\n", port)
+	log.Fatal(server.ListenAndServe())
+}
 
 func main() {
 	// Определение порта
 	port := os.Getenv("TODO_PORT")
+	defaultPort := 7540 // Порт по умолчанию
 	if port == "" {
-		port = strconv.Itoa(tests.Port) // Порт по умолчанию
+		port = strconv.Itoa(defaultPort)
 	}
 
 	// Определение директории для файлов
 	webDir := "../web"
 
-	// Создание HTTP сервера
-	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	// Создание экземпляра http.Server
+	httpServer := &http.Server{
+		Addr: ":" + port, // Установка адреса сервера
+	}
+	fmt.Println(httpServer.Addr)
 
-	// Запуск сервера на указанном порту
-	log.Printf("Сервер запущен на порту %v\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	// Запуск сервера
+	HttpServer(port, webDir, httpServer)
+
+	sigs := make(chan os.Signal, 1)
+	var sig os.Signal
+
+	for {
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGBUS, syscall.SIGSEGV)
+		sig = <-sigs
+		fmt.Println()
+		fmt.Println("signal:", sig)
+		if sig == syscall.SIGINT || sig == syscall.SIGTERM {
+			break
+		}
+	}
+
 }
