79cfab93b7c03f360121247dcf45bd6c022182d9
Maria Petrova <maria-petrova1296@yandex.ru>
2024-05-29 02:46:32 +0300

    Добавлены функция СalculatDailyTask, СalculatYearlyTask, СalculatWeeklyTask

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,6 +11,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 
@@ -47,6 +49,15 @@ func HttpServer(port, wd string) *http.Server {
 	return &httpServer
 }
 
+func OpenDataBase(tableName string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", tableName)
+	if err != nil {
+		fmt.Printf("Error opening database: %s\n", err)
+		return nil, err
+	}
+	return db, nil
+}
+
 func ChekingDataBase() error {
 	tableName := "scheduler.db"
 
@@ -66,26 +77,22 @@ func ChekingDataBase() error {
 		_, err = os.Stat(dbFile)
 		if err != nil {
 			fmt.Printf("Database file information missing: %s \nA new database file will be created... \n", err)
+			// Функция, которая открывает БД
+			db, err := OpenDataBase(tableName)
+			defer db.Close()
+			if err != nil {
+				fmt.Printf("%s", err)
+				return err
+			}
+			// Функция, которая создает таблицу и индекс
+			err = CreateTableWithIndex(db)
+			if err != nil {
+				fmt.Printf("%s", err)
+				return err
+			}
 		} else {
 			fmt.Println("The database file already exists")
 		}
-	}
-
-	var install bool
-	if err != nil {
-		install = true
-	}
-
-	if install == true {
-		// если install равен true, после открытия БД требуется выполнить
-		// sql-запрос с CREATE TABLE и CREATE INDEX
-		db, err := sql.Open("sqlite", tableName)
-		if err != nil {
-			fmt.Printf("Error opening database: %s", err)
-			return fmt.Errorf(err.Error())
-		}
-		defer db.Close()
-		CreateTableWithIndex(db)
 	}
 	return nil
 }
@@ -104,16 +111,113 @@ func CreateTableWithIndex(db *sql.DB) error {
 
 	_, err := db.Exec(createTableRequest)
 	if err != nil {
-		fmt.Printf("Error creating table: %s", err)
+		fmt.Printf("Error creating table: %s\n", err)
 		return err
 	}
 
 	_, err = db.Exec(createIndexRequest)
 	if err != nil {
-		fmt.Printf("Error creating index: %s", err)
+		fmt.Printf("Error creating index: %s\n", err)
 		return err
 	}
 	return nil
+}
+
+func NextDate(now time.Time, date string, repeat string) (string, error) {
+	// Парсин стартового-исходного времени, когда задача была выполнена первый раз
+	startDate, err := time.Parse("20060102", date)
+	if err != nil {
+		fmt.Printf("The start time cannot be converted to a valid date: %s", err)
+		return "", err
+	}
+
+	switch {
+	case strings.HasPrefix(repeat, "d "):
+
+		return СalculatDailyTask(now, startDate, repeat) // Функция расчета даты для ежедневных задач
+	case repeat == "y":
+		return СalculatYearlyTask(now, startDate) // Функция расчета даты для ежегодных дел
+	case strings.HasPrefix(repeat, "w "):
+		return СalculatWeeklyTask(now, startDate, repeat) // Функция расчета даты для задач на определенные дни недели
+	case strings.HasPrefix(repeat, "m "):
+		return "", nil // Функция расчета даты для задач на определенные дни месяца
+	case repeat == "":
+		return "", errors.New("Repeat is empty")
+	default:
+		return "", errors.New("Repetition rule is not supported")
+	}
+
+}
+
+func СalculatDailyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
+	days, err := strconv.Atoi(strings.TrimPrefix(repeat, "d "))
+	if err != nil {
+		fmt.Printf("Error converting string to number:%s \n", err)
+		return "", err
+	}
+
+	if days <= 0 || days > 400 {
+		err = errors.New("Invalid number of days")
+		return "", err
+	}
+
+	nextDate := startDate
+	for now.After(nextDate) || now == nextDate {
+		nextDate = nextDate.AddDate(0, 0, days)
+	}
+	return nextDate.Format("20060102"), nil
+}
+
+func СalculatYearlyTask(now time.Time, startDate time.Time) (string, error) {
+	nextDate := startDate.AddDate(1, 0, 0)
+	for now.After(nextDate) {
+		nextDate = nextDate.AddDate(1, 0, 0)
+	}
+	return nextDate.Format("20660102"), nil
+}
+
+func СalculatWeeklyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
+	// Удаляем префикс, чтобы осталась часть строки с числами
+	daysStr := strings.TrimPrefix(repeat, "w ")
+
+	// Пробуем разделить сроку по запятым, чтобы получить массив из строк
+	days := strings.Split(daysStr, ",")
+
+	// Создаем новый массив для целых чисел
+	var daysWeekNum []int
+
+	// В цикле перебираем ранее полученный строковый массив days
+	for _, dayStr := range days {
+		// Форматируем строки в целые числа и добавляем в новый числовой массив
+		dayNum, err := strconv.Atoi(dayStr)
+		if err != nil {
+			fmt.Printf("Error converting string to number: %s", err)
+		}
+		daysWeekNum = append(daysWeekNum, dayNum)
+	}
+
+	// Находим следующий день после текущего now
+	nextDate := startDate
+	for now.After(nextDate) || now == nextDate {
+		nextDate = nextDate.AddDate(0, 0, 1)
+	}
+
+	// Перебираем дни после найденного nextDate и сравниваем с числами из массива
+	// Заводим счетчик, чтобы цикл не выполнялся бесконечно
+	counter := 0
+	for nextDate.After(now) {
+		if counter >= 30 {
+			return "", errors.New("Next date for task not found")
+		}
+
+		for _, dayWeek := range daysWeekNum {
+			if dayWeek == int(nextDate.Weekday()) {
+				return nextDate.Format("20060102"), nil
+			}
+		}
+		nextDate = nextDate.AddDate(0, 0, 1)
+		counter++
+	}
 }
 
 func main() {
