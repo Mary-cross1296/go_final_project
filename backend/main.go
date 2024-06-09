4ed853db9f6214ef617b1dc09c93c69fe5e82433
Maria Petrova <maria-petrova1296@yandex.ru>
2024-06-09 19:08:50 +0300

    Все тесты для функции CalculatDayOfMonthTask завершаются корректно

--- backend/main.go
+++ backend/main.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/signal"
@@ -26,17 +27,27 @@ type Scheduler struct {
 	Repeat  string
 }
 
-func HttpServer(port, wd string) *http.Server {
+func HttpServer(port, wd string, handlers map[string]http.HandlerFunc) *http.Server {
 	// Создание объект сервера
 	httpServer := http.Server{
 		Addr: ":" + port, // Установка адреса сервера
 	}
 
 	// Определение обработчика для корневого пути
+	mux := http.NewServeMux()
 	requestHandler := http.FileServer(http.Dir(wd))
+	mux.Handle("/", requestHandler)
 
 	// Настройка сервера
-	httpServer.Handler = requestHandler
+	//httpServer.Handler = requestHandler
+
+	// Добавление пользовательских обработчиков
+	for path, handler := range handlers {
+		mux.Handle(path, handler)
+	}
+
+	// Присваивание mux полю Handler сервера
+	httpServer.Handler = mux
 
 	// Запуск сервера на указанном порту
 	log.Printf("Сервер запущен на порту %v\n", port)
@@ -97,6 +108,47 @@ func ChekingDataBase() error {
 	return nil
 }
 
+func CountNegativeNumbers(nums []int) int {
+	counterNegativeNum := 0
+	for _, num := range nums {
+		if num < 0 {
+			counterNegativeNum++
+		}
+	}
+	return counterNegativeNum
+}
+
+func FindMinNum(nums []int, numNegative int) int {
+	minNumDay := math.MaxInt64
+
+	if numNegative <= 1 {
+		for _, minDay := range nums {
+			if minDay > 0 && minDay < minNumDay {
+				minNumDay = minDay
+			}
+		}
+	} else if numNegative > 1 {
+		minNumDay = 0
+		for _, minDay := range nums {
+			if minDay < 0 && minDay < minNumDay {
+				minNumDay = minDay
+			}
+		}
+	}
+	return minNumDay
+}
+
+func CalculatAllegedNextDate(nextDate time.Time, day int) time.Time {
+	// Устанавливаем предполагаемую следующую дату задания на 1 число текущего месяца
+	allegedNextDate := time.Date(nextDate.Year(), nextDate.Month(), 1, 0, 0, 0, 0, nextDate.Location())
+	// Переносим на первое число следующего месяца
+	allegedNextDate = allegedNextDate.AddDate(0, 1, 0)
+	// Из полученной даты вычетаем указанное кол-во дней
+	// Получаем предполагаемую дату следующей задачи
+	allegedNextDate = allegedNextDate.AddDate(0, 0, day-1)
+	return allegedNextDate
+}
+
 func CreateTableWithIndex(db *sql.DB) error {
 	createTableRequest := `
 	CREATE TABLE scheduler (
@@ -123,34 +175,10 @@ func CreateTableWithIndex(db *sql.DB) error {
 	return nil
 }
 
-func NextDate(now time.Time, date string, repeat string) (string, error) {
-	// Парсин стартового-исходного времени, когда задача была выполнена первый раз
-	startDate, err := time.Parse("20060102", date)
-	if err != nil {
-		fmt.Printf("The start time cannot be converted to a valid date: %s", err)
-		return "", err
-	}
-
-	switch {
-	case strings.HasPrefix(repeat, "d "):
-
-		return СalculatDailyTask(now, startDate, repeat) // Функция расчета даты для ежедневных задач
-	case repeat == "y":
-		return СalculatYearlyTask(now, startDate) // Функция расчета даты для ежегодных дел
-	case strings.HasPrefix(repeat, "w "):
-		return СalculatWeeklyTask(now, startDate, repeat) // Функция расчета даты для задач на определенные дни недели
-	case strings.HasPrefix(repeat, "m "):
-		return "", nil // Функция расчета даты для задач на определенные дни месяца
-	case repeat == "":
-		return "", errors.New("Repeat is empty")
-	default:
-		return "", errors.New("Repetition rule is not supported")
-	}
-
-}
-
-func СalculatDailyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
+// Условие: задача повторяется каждый раз через заданное кол-во дней
+func CalculatDailyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
 	days, err := strconv.Atoi(strings.TrimPrefix(repeat, "d "))
+
 	if err != nil {
 		fmt.Printf("Error converting string to number:%s \n", err)
 		return "", err
@@ -162,21 +190,31 @@ func СalculatDailyTask(now time.Time, startDate time.Time, repeat string) (stri
 	}
 
 	nextDate := startDate
+	// Рассматриваем вариант, когда дата начала(starDate) задачи находится в будущем относительно текущего времени(now)
+	if now.Before(nextDate) || now == nextDate {
+		nextDate = nextDate.AddDate(0, 0, days)
+		return nextDate.Format("20060102"), nil
+	}
+
+	// Рассматриваем вариант, когда дата начала задачи(starDate) находится в прошлом относительно текущего времени(now)
 	for now.After(nextDate) || now == nextDate {
 		nextDate = nextDate.AddDate(0, 0, days)
 	}
+
 	return nextDate.Format("20060102"), nil
 }
 
-func СalculatYearlyTask(now time.Time, startDate time.Time) (string, error) {
+// Условие: перенос задачи на год вперед
+func CalculatYearlyTask(now time.Time, startDate time.Time) (string, error) {
 	nextDate := startDate.AddDate(1, 0, 0)
 	for now.After(nextDate) {
 		nextDate = nextDate.AddDate(1, 0, 0)
 	}
-	return nextDate.Format("20660102"), nil
+	return nextDate.Format("20060102"), nil
 }
 
-func СalculatWeeklyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
+// Условие: перенос задачи на один из указанных дней недели
+func CalculatWeeklyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
 	// Удаляем префикс, чтобы осталась часть строки с числами
 	daysStr := strings.TrimPrefix(repeat, "w ")
 
@@ -211,7 +249,7 @@ func СalculatWeeklyTask(now time.Time, startDate time.Time, repeat string) (str
 	// Заводим счетчик, чтобы цикл не выполнялся бесконечно
 	counter := 0
 	for nextDate.After(now) {
-		if counter >= 14 {
+		if counter > 8 {
 			return "", errors.New("Next date for task not found")
 		}
 
@@ -223,9 +261,11 @@ func СalculatWeeklyTask(now time.Time, startDate time.Time, repeat string) (str
 		nextDate = nextDate.AddDate(0, 0, 1)
 		counter++
 	}
+	return "", nil
 }
 
-func СalculatMonthlyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
+// Определение нужной функции для определения ежемесячных задач
+func CalculatMonthlyTask(now time.Time, startDate time.Time, repeat string) (string, error) {
 	daysMonthStr := strings.TrimPrefix(repeat, "m ")
 	daysMonth := strings.Split(daysMonthStr, " ")
 
@@ -241,9 +281,15 @@ func СalculatMonthlyTask(now time.Time, startDate time.Time, repeat string) (st
 				fmt.Printf("Error converting string to number: %s", err)
 				return "", err
 			}
-			daysNum = append(daysNum, dayNum)
+
+			if dayNum > -3 && dayNum < 32 {
+				daysNum = append(daysNum, dayNum)
+			} else {
+				return "", fmt.Errorf("number greater than 32 or less than -3")
+			}
 		}
-		СalculatDayOfMonthTask(now, startDate, daysNum)
+
+		return CalculatDayOfMonthTask(now, startDate, daysNum)
 	}
 
 	// Пока мы не знаем, какая перед нами комбинация
@@ -252,51 +298,274 @@ func СalculatMonthlyTask(now time.Time, startDate time.Time, repeat string) (st
 	numList1 := strings.Split(daysMonth[0], ",")
 	numList2 := strings.Split(daysMonth[1], ",")
 
-	// День + месяцы
-	if len(numList1) == 1 {
-		// Преобразуем единственный элемент первого массива в число
-		// Получаем день задачи
-		dayNum, _ := strconv.Atoi(numList1[0])
-
-		// Преобразуем элементы второго массива в числа
-		// Создаем новый числовой массив
-		var monthsNum []int
-		for _, month := range numList2 {
-			monthNum, err := strconv.Atoi(month)
-			if err != nil {
-				fmt.Printf("Error converting string to number: %s", err)
-				return "", err
-			}
-			monthsNum = append(monthsNum, monthNum)
+	var daysNum []int
+	for _, day := range numList1 {
+		dayNum, err := strconv.Atoi(day)
+		if err != nil {
+			fmt.Printf("Error converting string to number: %s", err)
+			return "", err
 		}
-		СalculatDayOfMonthsTask(now, startDate, dayNum, monthsNum)
+		daysNum = append(daysNum, dayNum)
+	}
+	// Преобразуем единственный элемент первого массива в число
+	// Получаем день задачи
+	//dayNum, _ := strconv.Atoi(numList1[0])
+
+	// Преобразуем элементы второго массива в числа
+	// Создаем новый числовой массив
+	var monthsNum []int
+	for _, month := range numList2 {
+		monthNum, err := strconv.Atoi(month)
+		if err != nil {
+			fmt.Printf("Error converting string to number: %s", err)
+			return "", err
+		}
+
+		if monthNum < 0 {
+			return "", fmt.Errorf("month cannot be a negative number")
+
+		}
+		monthsNum = append(monthsNum, monthNum)
 	}
 
+	return CalculatMonthsTask(now, startDate, daysNum, monthsNum)
+}
+
+// Условие: перенос задачи на заданный день месяца
+func CalculatDayOfMonthTask(now time.Time, startDate time.Time, daysNum []int) (string, error) {
+	// Считаем кол-во отрицательных чисел в массиве
+	negativeNum := CountNegativeNumbers(daysNum)
+
+	// Ищем мининимально число в массиве daysNum
+	minNumDay := FindMinNum(daysNum, negativeNum)
+
+	// Текущая(now) дата в прошлом относитительно даты старта(startDate)
+	nextDate := startDate
+	if now == startDate {
+		nextDate = startDate.AddDate(0, 0, 1)
+	}
+
+	for now.Before(nextDate) {
+		for _, day := range daysNum {
+			// Если день из массива положительное число и равен дню проверяемой даты, то назначаем выполнение задачи
+			if day > 0 && day == nextDate.Day() {
+				return nextDate.Format("20060102"), nil
+			}
+
+			if len(daysNum) == 1 && negativeNum == 1 {
+				allegedNextDate := CalculatAllegedNextDate(nextDate, day)
+
+				if nextDate.Day() <= allegedNextDate.Day() {
+					return allegedNextDate.Format("20060102"), nil
+				}
+			}
+
+			// Если день из массива отрицательное число при этом в массиве одно отрицательное число,
+			// то вычисляем следующую дату задачи
+			if day < 0 && negativeNum == 1 {
+				allegedNextDate := CalculatAllegedNextDate(nextDate, day)
+				fmt.Printf("отладка allegedNextDate %v", allegedNextDate)
+
+				if nextDate.Day() >= minNumDay &&
+					nextDate.Day() <= allegedNextDate.Day() &&
+					allegedNextDate.Day() <= minNumDay {
+					return allegedNextDate.Format("20060102"), nil
+				}
+			}
+
+			// Если день из массива отрицательное число (при этом в массиве два отрицательных числа),
+			// то вычисляем следующую дату задачи следующим образом
+			if day < 0 && negativeNum == 2 {
+				day1 := daysNum[0]
+				day2 := daysNum[1]
+
+				allegedNextDate1 := CalculatAllegedNextDate(nextDate, day1)
+				fmt.Printf("отладка allegedNextDate1 %v", allegedNextDate1)
+
+				allegedNextDate2 := CalculatAllegedNextDate(nextDate, day2)
+				fmt.Printf("отладка allegedNextDate2 %v", allegedNextDate2)
+
+				// Вычисляем дату, которая происходит раньше
+				if allegedNextDate1.Day() >= nextDate.Day() &&
+					allegedNextDate2.Day() > nextDate.Day() {
+					return allegedNextDate2.Format("20060102"), nil
+				} else {
+					return allegedNextDate1.Format("20060102"), nil
+				}
+			}
+		}
+		nextDate = nextDate.AddDate(0, 0, 1)
+	}
+	///////////////////////////////////////////////////////////////////////////////
+
+	// Текущая дата(now) в будущем относительно старта(startDate)
+	if now == startDate || now.After(startDate) {
+		nextDate = now.AddDate(0, 0, 1)
+	} else {
+		return "", fmt.Errorf("next date error")
+	}
+
+	// Перебираем дни после текущей даты и сравниваем с числом из массива дней
+	for nextDate.After(now) {
+		for _, day := range daysNum {
+			// Если день из массива положительное число и равен дню проверяемой даты, то назначаем выполнение задачи
+			if day > 0 && day == nextDate.Day() {
+				return nextDate.Format("20060102"), nil
+			}
+
+			// Если день из массива отрицательное число при этом в массиве одно отрицательное число,
+			// то вычисляем следующую дату задачи
+			if day < 0 && negativeNum == 1 {
+				allegedNextDate := CalculatAllegedNextDate(nextDate, day)
+				fmt.Printf("отладка allegedNextDate %v", allegedNextDate)
+
+				if nextDate.Day() >= minNumDay &&
+					nextDate.Day() <= allegedNextDate.Day() &&
+					allegedNextDate.Day() <= minNumDay {
+					return allegedNextDate.Format("20060102"), nil
+				}
+			}
+
+			// Если день из массива отрицательное число (при этом в массиве два отрицательных числа),
+			// то вычисляем следующую дату задачи следующим образом
+			if day < 0 && negativeNum == 2 {
+				day1 := daysNum[0]
+				day2 := daysNum[1]
+
+				allegedNextDate1 := CalculatAllegedNextDate(nextDate, day1)
+				fmt.Printf("отладка allegedNextDate1 %v", allegedNextDate1)
+
+				allegedNextDate2 := CalculatAllegedNextDate(nextDate, day2)
+				fmt.Printf("отладка allegedNextDate2 %v", allegedNextDate2)
+
+				// Вычисляем дату, которая происходит раньше
+				if allegedNextDate1.Day() >= nextDate.Day() &&
+					allegedNextDate2.Day() > nextDate.Day() {
+					return allegedNextDate2.Format("20060102"), nil
+				} else {
+					return allegedNextDate1.Format("20060102"), nil
+				}
+			}
+		}
+		nextDate = nextDate.AddDate(0, 0, 1)
+	}
 	return "", nil
 }
 
-func СalculatDayOfMonthTask(now time.Time, startDate time.Time, daysNum []int) (string, error) {
+// Условие: перенос задачи на определенное число указанных месяцев
+func CalculatMonthsTask(now time.Time, startDate time.Time, daysNum []int, monthsNum []int) (string, error) {
 	nowTime := now.Format("20060102")
 	nowDate, _ := time.Parse("20060102", nowTime)
 
 	// Находим следующий день после текущей даты now
 	nextDate := nowDate.AddDate(0, 0, 1)
 
-	// Перебираем дни после текущей даты и сравниваем с числом из массива дней
-	for nextDate.After(now) {
-		for _, day := range daysNum {
-			// Если день из массива равен дню проверяемой даты, то назначаем выполнение задачи
-			if day == nextDate.Day() {
-				return nextDate.Format("20060102"), nil
+	// Ищем подходящий месяц
+	for _, month := range monthsNum {
+		if month > 12 {
+			err := errors.New("The month was entered incorrectly or is greater than 12")
+			return "", err
+		}
+
+		if month == int(nextDate.Month()) {
+			break
+		}
+		nextDate = nextDate.AddDate(0, 1, 0)
+	}
+
+	// Предварительно находим минимальное положительно число в массиве dayNum
+	minNumDay := math.MaxInt64
+
+	for _, minDay := range daysNum {
+		if minDay > 0 && minDay < minNumDay {
+			minNumDay = minDay
+		}
+
+		// Перебираем дни после текущей даты и сравниваем с числом из массива дней
+		for nextDate.After(now) {
+			for _, day := range daysNum {
+				// Если день из массива положительное число и равен дню проверяемой даты, то назначаем выполнение задачи
+				if day == nextDate.Day() {
+					return nextDate.Format("20060102"), nil
+				}
+				// Если день из массива отрицательное число, то вичисляем следующую дату задачи
+				if day < 0 {
+					// Устанавливаем предполагаемую следующую дату задания на 1 число текущего месяца
+					allegedNextDate := time.Date(nowDate.Year(), nowDate.Month(), 1, 0, 0, 0, 0, now.Location())
+					// Переносим на первое число следующего месяца
+					allegedNextDate = nextDate.AddDate(0, 1, 0)
+					// Из полученной даты вычетаем указанное кол-во дней
+					// Получаем предполагаемую дату следующей задачи
+					allegedNextDate = nextDate.AddDate(0, 0, -day)
+
+					if nextDate.Day() > minNumDay && nextDate.Day() <= allegedNextDate.Day() {
+						return allegedNextDate.Format("20060102"), nil
+					}
+				}
+				nextDate = nextDate.AddDate(0, 0, 1)
 			}
-			nextDate = nextDate.AddDate(0, 0, 1)
 		}
 	}
 	return "", nil
 }
 
-func СalculatDayOfMonthsTask(now time.Time, startDate time.Time, dayNum int, monthsNum []int) (string, error) {
-	return "", nil
+func NextDate(now time.Time, date string, repeat string) (string, error) {
+	// Парсин стартового-исходного времени, когда задача была выполнена первый раз
+	startDate, err := time.Parse("20060102", date)
+	if err != nil {
+		fmt.Printf("The start time cannot be converted to a valid date: %s", err)
+		return "", err
+	}
+
+	switch {
+	case strings.HasPrefix(repeat, "d "):
+		return CalculatDailyTask(now, startDate, repeat) // Функция расчета даты для ежедневных задач
+	case repeat == "y":
+		return CalculatYearlyTask(now, startDate) // Функция расчета даты для ежегодных дел
+	case strings.HasPrefix(repeat, "w "):
+		return CalculatWeeklyTask(now, startDate, repeat) // Функция расчета даты для задач на определенные дни недели
+	case strings.HasPrefix(repeat, "m "):
+		return CalculatMonthlyTask(now, startDate, repeat) // Функция расчета даты для задач на определенные дни месяца
+	case repeat == "":
+		return "", errors.New("Repeat is empty")
+	default:
+		return "", errors.New("Repetition rule is not supported")
+	}
+}
+
+// Обработчик запросов на /api/nextdate.
+func NextDateHandler(w http.ResponseWriter, r *http.Request) {
+	// Получаем Get-параметры из запроса
+	nowTime := r.FormValue("now")
+	date := r.FormValue("date")
+	repeat := r.FormValue("repeat")
+	//nowTime := r.URL.Query().Get("now")
+	//date := r.URL.Query().Get("date")
+	//repeat := r.URL.Query().Get("repeat")
+
+	// Преобразуем параметр "now" в формат time.Time
+	now, err := time.Parse("20060102", nowTime)
+	if err != nil {
+		http.Error(w, "Invalid 'now' parameter format. Use YYYYMMDD", http.StatusBadRequest)
+		return
+	}
+
+	// Вызываем функцию NextDate для получения следующей даты
+	nextDate, err := NextDate(now, date, repeat)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Возвращаем следующую дату задачи
+	fmt.Printf("Next date: %s \n", nextDate)
+
+	// Отправляем следующий ответ клиенту
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(nextDate))
+
+	// Отладочное сообщение
+	log.Printf("Received request for next date. Now: %s, Date: %s, Repeat: %s. Next date: %s", nowTime, date, repeat, nextDate)
 }
 
 func main() {
@@ -310,9 +579,17 @@ func main() {
 	// Определение директории для файлов
 	webDir := "../web"
 
+	// Мапинг обработчиков
+	handlers := map[string]http.HandlerFunc{
+		"/api/nextdate": NextDateHandler,
+	}
+
 	// Запуск сервера
-	server := HttpServer(port, webDir)
+	server := HttpServer(port, webDir, handlers)
 	ChekingDataBase()
+
+	// Устанавливаем обработчик для api/nextdate
+	http.HandleFunc("/api/nextdate", NextDateHandler)
 
 	// Создание канала для поступления сигналов
 	sigs := make(chan os.Signal, 1)
