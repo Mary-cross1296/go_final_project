e0b8df73ebe4aa333dd3a8fd949ea8e36b21d6db
Maria Petrova <maria-petrova1296@yandex.ru>
2024-07-23 00:17:17 +0300

    Тесты проходят из докер контейнера

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../backend/scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiJDJhJDEwJEY4V01HSXU1eU5mWmpTMFdIdnY4NHVLSUZwL0NaSG42eGZIM1Ewa2RIWTNDWEFSMnM3Z2ZhIiwiZXhwIjoxNzIxNTk5OTkyfQ.9ZtuKXlzdPA-S_oliQl7GtEm2SuZ0eQtDmm2LhG4pI0`
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiJDJhJDEwJC5DaXU5Y1cuaDVTdVNUS0ZoVUZYT3U2SWZjZVVRbE9wZDkwS2xwTEcwSFFOcE41RHlpemQuIiwiZXhwIjoxNzIxNzEwMDc3fQ.ed_Rg5EL645GPRlYRvIAtwWK2jRAk72WMIBvFIkuo_0`
