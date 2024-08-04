da66957c4b9fbb80924a435aea97457bc45edffc
Maria Petrova <maria-petrova1296@yandex.ru>
2024-08-04 05:54:37 +0300

    Изменение докер файла

--- tests/settings.go
+++ tests/settings.go
@@ -1,22 +1,7 @@
 package tests
 
-import (
-	"log"
-	"path/filepath"
-)
-
 var Port = 7540
-var DBFile = getDBFilePath()
+var DBFile = "../backend/scheduler.db"
 var FullNextDate = true
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiJDJhJDEwJGRsdllwWWdwYmxvT2VTVEpCa0Q3WGViaktGc0NVN3RqNk9CTVEuVi9DMDdRMmVpOWIxWno2IiwiZXhwIjoxNzIyNDk4Nzc1fQ.EoSRi18aMO9Ly5-tETDguTkMw5m2L2Yy22jhAJ1Pto0`
-
-func getDBFilePath() string {
-	dbFile := "../backend/scheduler.db"
-	absPath, err := filepath.Abs(dbFile)
-	if err != nil {
-		log.Fatalf("Error getting absolute path: %v", err)
-	}
-	log.Printf("Using database file: %s", absPath)
-	return absPath
-}
+var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXNzd29yZF9oYXNoIjoiJDJhJDEwJGg5TS5HOVhaaTJ5NXg2S2RyQTdOVk9GMk5QdXdGNS9ETC5hQWk5MGs0T1NHY01EbktVaWZHIiwiZXhwIjoxNzIyNTM3Mjg0fQ.fNC0OeO4SVBf22Lr2jiQNQ2h5bjt_zNOkrK20T7rQkI`
