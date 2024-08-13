998cbef38a7638fa3e29ce3c883c2c2f6fd2b0c0
Maria Petrova <maria-petrova1296@yandex.ru>
2024-08-13 22:33:20 +0300

    Рабочая версия после исправления замечаний. Добавить дефолтный пароль и токен для тестов

--- backend/main.go
+++ backend/main.go
@@ -1,23 +1,14 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"log"
-	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
-	"time"
 
 	"github.com/Mary-cross1296/go_final_project/api"
 	"github.com/Mary-cross1296/go_final_project/config"
 	"github.com/Mary-cross1296/go_final_project/storage"
 	"github.com/Mary-cross1296/go_final_project/utils"
 )
-
-// Глобальная переменная для подключения к базе данных
-var Db *sql.DB
 
 const TableName = "scheduler.db"
 
@@ -47,14 +38,17 @@ func main() {
 	storage.ChekingDataBase(TableName)
 
 	// Инициализация базы данных
-	Db, err := storage.OpenDataBase(TableName)
+	db, err := storage.OpenDataBase(TableName)
 	if err != nil {
 		log.Fatalf("Main(): Error opening database: %s\n", err)
 	}
-	defer Db.Close()
+	defer db.Close()
 
-	// Запуск сервера
-	server := api.HttpServer(port, webDir, Db)
+	// Создание и запуск сервера
+	_, err = api.HttpServer(port, webDir, db)
+	if err != nil {
+		log.Fatalf("Main(): Error starting server: %s\n", err)
+	}
 
 	// Получение и обновление токена
 	if err := utils.GetAndUpdateToken(); err != nil {
@@ -63,22 +57,6 @@ func main() {
 		log.Println("Token updated successfully")
 	}
 
-	// Создание канала для поступления сигналов
-	sigs := make(chan os.Signal, 1)
-	var sig os.Signal
-	for {
-		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-		sig = <-sigs
-		log.Println()
-		log.Println("signal:", sig)
-		if sig == syscall.SIGINT || sig == syscall.SIGTERM {
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			defer cancel()
-			if err := server.Shutdown(ctx); err != nil {
-				log.Printf("Error during stop: %v\n", err)
-			}
-			log.Printf("Server stopped correctly")
-			break
-		}
-	}
+	// Ожидание завершения работы сервера
+	select {}
 }
